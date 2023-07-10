4b80d20220e0561add1067e8470a4ab3680610e9
Tyler Vick <jrtvick@gmail.com>
2023-07-10 17:52:02 -0400

    finished styling the about me section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header>
+      <div>
       <h1>Tyler Vick</h1>
       <nav>
         <ul>
-          <li>About Me</li>
-          <li>Projects</li>
+          <li><Link to="/">About Me</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
           <li>Resume</li>
           <li>Contact</li>
         </ul>
       </nav>
+      </div>
     </header>
   )
 }
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+import picOfMe from "../images/picOfMe.jpeg";
+
 export default function AboutMe() {
   return (
-    <div
-    style={{
-        fontSize: '10rem'
-    }}
-    >AboutMe</div>
-  )
+    <section>
+      <div className="about-me-content">
+        <div>
+          <img src={picOfMe} />
+          {/* BFJHWOIEROIHJ */}
+        </div>
+        <div className="bio-text">
+          <h1>About Me</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia
+            ipsam at asperiores autem cumque voluptatem amet quidem voluptatum
+            quam non. Tenetur sequi veritatis voluptas officiis placeat aliquam
+            earum! Voluptatem quidem quaerat nisi commodi quos. Distinctio quo
+            eligendi voluptatum quas voluptatem eius, ut accusantium quia esse
+            quidem alias dolorem vitae sequi.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
