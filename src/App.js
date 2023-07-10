2a61f8dfa25b8fea52678a48d789f362968dcfa8
Tyler Vick <jrtvick@gmail.com>
2023-07-10 16:20:44 -0400

    started structuring portfolio in the browser, added colour and spacing to the header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div id="main-parent">
+      <Header />
+      <main>blwejgwoigh</main>
+      <footer>wgoegj</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <h1>Tyler Vick</h1>
+      <nav>
+        <ul>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Resume</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
