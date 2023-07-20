cdc93d5055d6872df76cc7c00051b1c9166d140a
Tyler Vick <jrtvick@gmail.com>
2023-07-20 02:24:30 -0400

    edited about me text

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -15,7 +15,7 @@ export default function AboutMe() {
           My name is Tyler Vick. I am 32 years old and I live in Toronto, ON. I started my coding career with a fullstack bootcamp in 2023, and am continuing to self teach and learn new things all the time!
 <br></br>
 <br></br>
-          Outside of web development, I like to spend my time reading, watching movies, cheering on the Detroit Red Wings, and playing 90s JRPGs.
+          Outside of web development, I like to spend my time reading, watching movies, cheering on the Detroit Red Wings, and playing '90s JRPGs.
           </p>
         </div>
       </div>
